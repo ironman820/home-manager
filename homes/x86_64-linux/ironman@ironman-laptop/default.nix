98847bdbfa4b8ab378c718c8c19feeb57c772eee
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-11-12 15:42:41 -0600

    Remove home alarm config

--- homes/x86_64-linux/ironman@ironman-laptop/default.nix
+++ homes/x86_64-linux/ironman@ironman-laptop/default.nix
@@ -16,37 +16,4 @@ in
     };
     suites.workstation = enabled;
   };
-  # systemd.user = {
-  #   services = {
-  #     "gotobedweekdays" = {
-  #       Unit.Description = "myalarm Go to bed";
-  #       Install.WantedBy = [ "default.target" ];
-  #       Service.ExecStart = ''${blockyalarm}/bin/blockyalarm "Go To Bed!"'';
-  #     };
-  #     "gotobedweekends" = {
-  #       Unit.Description = "myalarm Go to bed";
-  #       Install.WantedBy = [ "default.target" ];
-  #       Service.ExecStart = ''${blockyalarm}/bin/blockyalarm "Go To Bed!"'';
-  #     };
-  #     "shouldhaveleft" = {
-  #       Unit.Description = "myalarm Leave!";
-  #       Install.WantedBy = [ "default.target" ];
-  #       Service.ExecStart = ''${blockyalarm}/bin/blockyalarm "Leave for Work"'';
-  #     };
-  #   };
-  #   timers = {
-  #     "gotobedweekdays" = {
-  #       Install.WantedBy = [ "timers.target" ];
-  #       Timer.OnCalendar = "Mon..Thu,Sun 22:00,15:00";
-  #     };
-  #     "gotobedweekends" = {
-  #       Install.WantedBy = [ "timers.target" ];
-  #       Timer.OnCalendar = "Fri,Sat 23:00,15:00";
-  #     };
-  #     "shouldhaveleft" = {
-  #       Install.WantedBy = [ "timers.target" ];
-  #       Timer.OnCalendar = "Mon..Fri 07:10,20:00";
-  #     };
-  #   };
-  # };
 }
