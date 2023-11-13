f5dc6c5812f29d2650573b9fb0ac4873dec3d80e
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-11-13 13:37:24 -0600

    Tiling Keys

--- modules/home/qtile/config/settings/keys.py
+++ modules/home/qtile/config/settings/keys.py
@@ -3,17 +3,25 @@ from libqtile.command import lazy
 
 
 browser: str = "brave"
-rofi:str = "rofi -show drun -show-icons"
-terminal:str = "kitty"
+rofi: str = "rofi -show drun -show-icons"
+terminal: str = "kitty"
 ranger = f"{terminal} bash -c ranger ~"
-mod:str = "mod4"
+mod: str = "mod4"
 
 keys = [
     Key([], "XF86AudioMute", lazy.widget["pulsevolume"].mute()),
     Key([], "XF86AudioLowerVolume", lazy.widget["pulsevolume"].decrease_vol()),
     Key([], "XF86AudioRaiseVolume", lazy.widget["pulsevolume"].increase_vol()),
-    Key([], "XF86MonBrightnessDown", lazy.widget["backlight"].change_backlight('down')),
-    Key([], "XF86MonBrightnessUp", lazy.widget["backlight"].change_backlight('up')),
+    Key(
+        [],
+        "XF86MonBrightnessDown",
+        lazy.widget["backlight"].change_backlight("down"),
+    ),
+    Key(
+        [],
+        "XF86MonBrightnessUp",
+        lazy.widget["backlight"].change_backlight("up"),
+    ),
     Key([mod], "f", lazy.spawn(ranger), desc="Move focus to left"),
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
@@ -24,14 +32,55 @@ keys = [
     Key([mod], "r", lazy.spawn(rofi), desc="Spawn a command using rofi"),
     Key([mod], "t", lazy.spawn(terminal), desc="Launch terminal"),
     Key([mod], "w", lazy.spawn(browser), desc="Launch browser"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key(
+        [mod],
+        "space",
+        lazy.layout.next(),
+        desc="Move window focus to other window",
+    ),
+    Key(
+        [mod, "shift"],
+        "h",
+        lazy.layout.shuffle_left(),
+        desc="Move window to the left",
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
+    Key(
+        [mod, "shift"],
+        "j",
+        lazy.layout.shuffle_down(),
+        desc="Move window down",
+    ),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key(
+        [mod, "shift"],
+        "n",
+        lazy.window.toggle_floating(),
+        desc="Toggle Floating window",
+    ),
+    Key(
+        [mod, "control"],
+        "h",
+        lazy.layout.grow_left(),
+        desc="Grow window to the left",
+    ),
+    Key(
+        [mod, "control"],
+        "l",
+        lazy.layout.grow_right(),
+        desc="Grow window to the right",
+    ),
+    Key(
+        [mod, "control"],
+        "j",
+        lazy.layout.grow_down(),
+        desc="Grow window down",
+    ),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key(
         [mod, "shift"],
