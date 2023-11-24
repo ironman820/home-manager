a1f2c4fa5d61388ff4849b07baca25fb393f3a25
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-11-23 18:40:02 -0600

    Update UFO and git settings

--- modules/home/programs/nvim/config/lua/settings/ufo.lua
+++ modules/home/programs/nvim/config/lua/settings/ufo.lua
@@ -3,31 +3,17 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+local keys = vim.keymap
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+keys.set('n', 'zR', require('ufo').openAllFolds)
+keys.set('n', 'zM', require('ufo').closeAllFolds)
+keys.set('n', '<Leader>-', '<Cmd>set foldlevel-=1<CR>zz', { desc = "zoom out (see less)", })
+keys.set('n', '<Leader>=', '<Cmd>set foldlevel+=1<CR>zz', { desc = "zoom in (see more)", })
+keys.set('n', '<Leader><Leader>-', '<Cmd>set foldlevel=0<CR>zM', { desc = "zoom out max", })
+keys.set('n', '<Leader><Leader>=', '<Cmd>set foldlevel=20<CR>zR', { desc = "zoom in max", })
+keys.set('n', 'Z', 'zkzxzz', {})
+keys.set('n', 'X', 'zjzxzz', {})
 
--- Option 2: nvim lsp as LSP client
--- Tell the server the capability of foldingRange,
--- Neovim hasn't added foldingRange to default capabilities, users must add it manually
--- local capabilities = vim.lsp.protocol.make_client_capabilities()
--- capabilities.textDocument.foldingRange = {
---     dynamicRegistration = false,
---     lineFoldingOnly = true
--- }
--- local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
--- for _, ls in ipairs(language_servers) do
---     require('lspconfig')[ls].setup({
---         capabilities = capabilities
---         -- you can add other fields for setting up lsp server in this table
---     })
--- end
--- require('ufo').setup()
---
-
--- Option 3: treesitter as a main provider instead
--- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
--- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
 require("ufo").setup({provider_selector = function(_, _, _)
     return {'treesitter', 'indent'}
 end})
