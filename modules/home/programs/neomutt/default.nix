976e5caccf9f9caedd713775a63f55c6bd132e42
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-11-28 12:40:31 -0600

    Update neomutt signature settings

--- modules/home/programs/neomutt/default.nix
+++ modules/home/programs/neomutt/default.nix
@@ -5,7 +5,9 @@ let
   inherit (lib.ironman) enabled;
 
   cfg = config.ironman.home.programs.neomutt;
-  modFolder = "${config.xdg.configHome}/home-manager/modules/home/programs/neomutt";
+  modFolder =
+    "${config.xdg.configHome}/home-manager/modules/home/programs/neomutt";
+  configFolder = "${config.xdg.configHome}/mutt";
 in {
   options.ironman.home.programs.neomutt = {
     enable = mkEnableOption "Install Neomutt";
@@ -13,70 +15,75 @@ in {
     workEmail = mkEnableOption "Setup Work Email";
   };
 
-  config = mkIf cfg.enable (let
-      sopsFile = ./secrets/neomutt.yaml;
-    in {
-      ironman.home.sops.secrets = mkMerge [
-        (mkIf cfg.personalEmail {
-          "mbsync_personal_email" = {
-            inherit sopsFile;
-            path = "${config.home.homeDirectory}/.mbsyncrc";
-          };
-          "muttrc_personal_email" = {
-            inherit sopsFile;
-            path = "${config.xdg.configHome}/mutt/accounts/master.muttrc";
-          };
-          "muttrc_work_email" = {
-            inherit sopsFile;
-            path = "${config.xdg.configHome}/mutt/accounts/work.muttrc";
-          };
-          "msmtp_personal_config" = {
-            inherit sopsFile;
-            path = "${config.xdg.configHome}/msmtp/config";
-          };
-        })
-        (mkIf cfg.workEmail {
-          "mbsync_work_email" = {
-            inherit sopsFile;
-            path = "${config.home.homeDirectory}/.mbsyncrc";
-          };
-          "muttrc_work_email" = {
-            inherit sopsFile;
-            path = "${config.xdg.configHome}/mutt/accounts/master.muttrc";
-          };
-          "msmtp_work_config" = {
-            inherit sopsFile;
-            path = "${config.xdg.configHome}/msmtp/config";
-          };
-        })
-      ];
-      home = {
-        packages = with pkgs; [
-          abook
-          browsh
-          cacert
-          curl
-          fim
-          gettext
-          isync
-          lynx
-          mutt-wizard
-          msmtp
-          notmuch
-          pass
-          urlview
-        ];
-        shellAliases = {
-          mail = "neomutt";
+  config = mkIf cfg.enable (let sopsFile = ./secrets/neomutt.yaml;
+  in {
+    ironman.home.sops.secrets = mkMerge [
+      {
+        "mbsync" = {
+          inherit sopsFile;
+          path = "${config.home.homeDirectory}/.mbsyncrc";
         };
-      };
-      programs.neomutt = enabled;
-      xdg.configFile = {
-        "mutt/mailcap".source = mkOutOfStoreSymlink "${modFolder}/mailcap";
-        "mutt/muttrc".source = mkOutOfStoreSymlink "${modFolder}/muttrc";
-        "mutt/mutt-wizard.muttrc".source = mkOutOfStoreSymlink "${modFolder}/mutt-wizard.muttrc";
-        "mutt/switch.muttrc".source = mkOutOfStoreSymlink "${modFolder}/switch.muttrc";
-        "mutt/theme".source = "${pkgs.catppuccin-neomutt}/catppuccin-neomutt";
-      };
-    });
+        "msmtp_config" = {
+          inherit sopsFile;
+          path = "${config.xdg.configHome}/msmtp/config";
+        };
+        "work_sig" = {
+          inherit sopsFile;
+          path = "${configFolder}/signatures/work.sig";
+        };
+        "personal_sig" = {
+          inherit sopsFile;
+          path = "${configFolder}/signatures/personal.sig";
+        };
+      }
+      (mkIf cfg.personalEmail {
+        "muttrc_personal_email" = {
+          inherit sopsFile;
+          path = "${configFolder}/accounts/master.muttrc";
+        };
+        "muttrc_work_email" = {
+          inherit sopsFile;
+          path = "${configFolder}/accounts/work.muttrc";
+        };
+      })
+      (mkIf cfg.workEmail {
+        "muttrc_work_email" = {
+          inherit sopsFile;
+          path = "${configFolder}/accounts/master.muttrc";
+        };
+        "muttrc_personal_email" = {
+          inherit sopsFile;
+          path = "${configFolder}/accounts/personal.muttrc";
+        };
+      })
+    ];
+    home = {
+      packages = with pkgs; [
+        abook
+        browsh
+        cacert
+        curl
+        fim
+        gettext
+        isync
+        lynx
+        mutt-wizard
+        msmtp
+        notmuch
+        pass
+        urlview
+      ];
+      shellAliases = { mail = "neomutt"; };
+    };
+    programs.neomutt = enabled;
+    xdg.configFile = {
+      "mutt/mailcap".source = mkOutOfStoreSymlink "${modFolder}/mailcap";
+      "mutt/muttrc".source = mkOutOfStoreSymlink "${modFolder}/muttrc";
+      "mutt/mutt-wizard.muttrc".source =
+        mkOutOfStoreSymlink "${modFolder}/mutt-wizard.muttrc";
+      "mutt/switch.muttrc".source =
+        mkOutOfStoreSymlink "${modFolder}/switch.muttrc";
+      "mutt/theme".source = "${pkgs.catppuccin-neomutt}/catppuccin-neomutt";
+    };
+  });
 }
