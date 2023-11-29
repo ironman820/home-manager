09e260cd2cad26ada0970371a6505b25246e7e56
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-11-29 06:42:33 -0600

    Add Zathura to Qtile

--- modules/home/qtile/default.nix
+++ modules/home/qtile/default.nix
@@ -18,6 +18,7 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       flameshot
+      zathura
     ];
     xdg.configFile."qtile/autostart.sh".source =
       writeShellScript "autostart.sh" ''
