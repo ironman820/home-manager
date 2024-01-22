62f7b251502a4d078493d11c8fb60a8e33b10401
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-01-21 23:24:44 -0600

    Migrate just file to nix

--- modules/home/just/default.nix
+++ modules/home/just/default.nix
@@ -1,16 +1,63 @@
-{ pkgs, config, lib, ... }:
-let
-  inherit (lib) mkIf;
-  inherit (lib.ironman) mkBoolOpt;
+{
+  pkgs,
+  config,
+  host,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf mkMerge;
+  inherit (lib.ironman) mkBoolOpt mkIfElse;
 
   cfg = config.ironman.home.just;
 in {
-  options.ironman.home.just = { enable = mkBoolOpt true "Install Just"; };
+  options.ironman.home.just = {enable = mkBoolOpt true "Install Just";};
 
   config = mkIf cfg.enable {
     home = {
-      file.".justfile".source = ./justfile;
-      packages = with pkgs; [ just ];
+      file.".justfile".text = mkMerge [
+        ''
+          default:
+            @just --list
+
+          apps:
+            flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+            flatpak install -uy com.usebottles.bottles
+            flatpak install -uy com.github.tchx84.Flatseal
+            flatpak install -uy org.gnome.gitlab.YaLTeR.VideoTrimmer
+
+          bios:
+            systemctl reboot --firmware-setup
+
+          switch:
+            flake switch ~/.config/flake#
+        ''
+        (
+          mkIfElse (host == "e105-laptop" || host == "ironman-laptop")
+          ''
+            home-switch:
+              home-manager switch --flake $(find ~/.config/home-manager/branches -maxdepth 1 -type d ! \( -name '.*' -o -name '*branches' \) -print | fzf)
+              systemctl --user restart sops-nix.service
+          ''
+          ''
+            home-switch:
+              home-manager switch
+              systemctl --user restart sops-nix.service
+          ''
+        )
+        ''
+          update:
+            #!/usr/bin/env bash
+            cd ~/.config/flake
+            flake update
+            flake switch
+            cd ~/.config/home-manager
+            flake update
+            home-manager switch
+            systemctl --user restart sops-nix.service
+            flatpak update -y
+        ''
+      ];
+      packages = with pkgs; [just];
       shellAliases = {
         "hs" = "just home-switch";
         "js" = "just switch";
@@ -19,4 +66,3 @@ in {
     };
   };
 }
-
